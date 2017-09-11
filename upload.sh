e95488dd08a5026175ed80dc7563b322c5f72d70
Tevin Jeffrey <tev.jeffrey@gmail.com>
2017-09-10 21:45:30 -0400

    Add sleep loop

--- upload.sh
+++ upload.sh
@@ -9,6 +9,23 @@
 # export GCS_BUCKET
 # export GCS_PROJECT
 
-pg_dump -v -w -c | gzip -9 > $PGDATABASE.sql.gz
-FILE_NAME="$PGDATABASE-$(date +"%Y%m%d%H%M")"
-gcsupload -bucket "$GCS_BUCKET" -project "$GCS_PROJECT" -name "$FILE_NAME" -source "$PGDATABASE.sql.gz"
+# export SLEEP_SECONDS
+
+backup(){
+    pg_dump -v -w -c | gzip -9 > $PGDATABASE.sql.gz
+    FILE_NAME="$PGDATABASE-$(date +"%Y%m%d%H%M")"
+    gcsupload -bucket "$GCS_BUCKET" -project "$GCS_PROJECT" -name "$FILE_NAME" -source "$PGDATABASE.sql.gz"
+}
+
+if [ -z $SLEEP_SECONDS ]; then
+    backup
+else
+    for (( ; ; ))
+    do
+        backup
+        sleep $SLEEP_SECONDS
+    done
+fi
+
+
+
