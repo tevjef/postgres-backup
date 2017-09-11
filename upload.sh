5ab9479515e7f04fbcaa05b1f2665877f713d77e
Tevin Jeffrey <tev.jeffrey@gmail.com>
2017-09-10 22:02:07 -0400

    Add logging

--- upload.sh
+++ upload.sh
@@ -12,9 +12,11 @@
 # export SLEEP_SECONDS
 
 backup(){
+    echo "Starting backup..."
     pg_dump -v -w -c | gzip -9 > $PGDATABASE.sql.gz
     FILE_NAME="$PGDATABASE-$(date +"%Y%m%d%H%M")"
     gcsupload -bucket "$GCS_BUCKET" -project "$GCS_PROJECT" -name "$FILE_NAME" -source "$PGDATABASE.sql.gz"
+    echo "Backup complete..."
 }
 
 if [ -z $SLEEP_SECONDS ]; then
@@ -23,6 +25,7 @@ else
     for (( ; ; ))
     do
         backup
+        echo "Sleeping for $SLEEP_SECONDS seconds.."
         sleep $SLEEP_SECONDS
     done
 fi
